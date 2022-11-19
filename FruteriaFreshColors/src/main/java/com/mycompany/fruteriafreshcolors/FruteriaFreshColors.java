9c5d3b26fff93b3facaa7258461ebc580c11985d
ChristoperBA <bricenoc506@gmail.com>
2022-11-18 18:28:18 -0600

    Primer Commit

---
+++ FruteriaFreshColors/src/main/java/com/mycompany/fruteriafreshcolors/FruteriaFreshColors.java
@@ -0,0 +1,10 @@
+
+
+package com.mycompany.fruteriafreshcolors;
+
+public class FruteriaFreshColors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
