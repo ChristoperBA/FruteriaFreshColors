4d487967293c530c38c648ba22938957a3ea6d7f
Gono27 <59101439+Gono27@users.noreply.github.com>
2022-12-01 21:31:45 -0600

    Hace falta conectarlos al proyecto principal
    No pude correr el principal asi que falta conectarlo

---
+++ FruteriaFreshColors/src/test/java/com/mycompany/fruteriafreshcolors/Conexion.java
@@ -0,0 +1,43 @@
+
+package com.mycompany.fruteriafreshcolors;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    String bd="FruteriaFreshColors";
+    String url="jdbc:mysql://localhost.:3306/";
+    String user="root";
+    String password="Davidmeneses1";
+    String driver="com.mysql.cj.jdbc.Driver";
+    Connection cx;
+    String mensaje;
+
+    public Conexion() {
+    }
+    
+    public Connection conectar(){
+        try {
+            Class.forName(driver);
+            cx=DriverManager.getConnection(url+bd, user, password);
+             JOptionPane.showMessageDialog(null, "Se logro la conexion con la base "+bd);
+        } catch (SQLException |ClassNotFoundException ex) {
+            //JOptionPane.showMessageDialog(null, "No se logro la conexion con la base  "+bd);
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cx;
+    }
+    public void desconectar(){
+        try {
+            cx.close();
+        } catch (SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+        }
+    } 
+    
+    
+}
